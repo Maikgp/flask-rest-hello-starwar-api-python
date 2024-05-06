2ebed527af6b691fa6e117f6dfc1a23e9eafa1be
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-05-06 18:27:32 +0000

    LISTO

--- src/app.py
+++ src/app.py
@@ -69,7 +69,67 @@ def get_user_profile(id_user):
         return jsonify({'message': 'Usuario no encontrado'}), 404
     
 
-#Planetas
+
+#PERSONAJES
+
+@app.route('/people', methods=['GET'])
+def get_personajes():
+
+    people = Personajes.query.all()
+
+    response_body = [item.serialize() for item in people]
+
+    return jsonify(response_body), 200
+
+
+@app.route('/people/<int:id_personajes>', methods=['GET'])
+def get_personaje(id_personajes):
+    people = Personajes.query.get(id_personajes)
+
+    if people:
+        return jsonify(people.serialize()), 200
+    else:
+        return jsonify({'message': 'Usuario no encontrado'}), 404
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])  
+def agregar_people_favorito(id_personajes):
+    
+    user = User.query.get(id_user)
+    if user is None:
+        return jsonify({'error': 'Usuario no encontrado'}), 404
+    
+    
+    personaje = Personajes.query.get(id_personajes)
+    if personaje is None:
+        return jsonify({'error': 'Personaje no encontrado'}), 404
+
+    favorito = Favoritos(id_user=user.id, id_personjes=id_people)
+    db.session.add(favorito)
+    db.session.commit()
+
+    return jsonify({'message': f'Personaje {people.name} agregado a favoritos del usuario {user.name}'}), 200 
+
+
+
+
+@app.route('/people/<int:id>', methods=['DELETE'])
+def handle_delete_people(id):
+    new_people = Personajes.query.get(id)
+    db.session.delete(new_people)
+    db.session.commit()
+
+    for favorite in new_people.favorites:
+        db.session.delete(favorite)
+
+    response_body = {
+       "msg":"personaje eliminado"
+    }
+    return jsonify(response_body), 200
+
+    
+
+#PLANETS
 
 @app.route('/planets', methods=['GET'])
 def get_planets():
@@ -94,30 +154,35 @@ def get_planeta(id_planeta):
 @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])  #revisar que aqui hay que hacer el favorite planet planet id
 def agregar_planeta_favorito(planet_id):
     
-    user = User.query.get(user_id)
+    user = User.query.get(id_user)
     if user is None:
         return jsonify({'error': 'Usuario no encontrado'}), 404
     
-    # Buscar el planeta en la base de datos
+    
     planeta = Planetas.query.get(planet_id)
     if planeta is None:
         return jsonify({'error': 'Planeta no encontrado'}), 404
 
-    # Crear un nuevo favorito y asociarlo al usuario y al planeta
-    favorito = Favoritos(user_id=user.id, planeta_id=planeta.id)
+    favorito = Favoritos(id_user=user.id, planeta_id=planeta.id)
     db.session.add(favorito)
     db.session.commit()
 
-    return jsonify({'message': f'Planeta {planeta.name} agregado a favoritos del usuario {user.name}'}), 200    
-    
+    return jsonify({'message': f'Planeta {planeta.name} agregado a favoritos del usuario {user.name}'}), 200   
 
+@app.route('/planets/<int:id>', methods=['DELETE'])
+def handle_delete_planet(id):
+    new_planet = Planetas.query.get(id)
+    db.session.delete(new_planet)
+    db.session.commit()
 
-    
+    for favorite in new_planet.favorites:
+        db.session.delete(favorite)
 
+    response_body = {
+       "msg":"planeta eliminado"
+    }
+    return jsonify(response_body), 200
 
-
-
-     
 
 
 
